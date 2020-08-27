261d9db3cabeedf67b575c88dea40da161b4eb92
Jack <52207916+jackkuma@users.noreply.github.com>
2020-08-27 22:58:01 +0800

    add change State

--- src/components/model/ModelMenu.js
+++ src/components/model/ModelMenu.js
@@ -6,11 +6,17 @@ import './menu.scss';
 const MenuButton = () => {
     const [detailVisible, setDetailVisible] = useState(false);
     const toggleMenu = () => setDetailVisible(!detailVisible);
+    
+    window.changeComponentState = (detailVisible) => {
+        setDetailVisible(!detailVisible);
+    }
 
     return(
         <Fragment>
             <Button icon={detailVisible ? 'close' : 'menu'} stylingMode="text" onClick={toggleMenu} />
-            <div id="menuList" className={["menu-penal", detailVisible ? "menuShow" : "menuHide"].join(' ')} />
+            <div id="menuList" 
+             className={["menu-penal", detailVisible ? "menuShow" : "menuHide"].join(' ')} 
+            />
         </Fragment>
     );
 };
