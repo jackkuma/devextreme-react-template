e909a3cf698a233dc8ea046aa4b6f0b4c6e939b1
Jack <52207916+jackkuma@users.noreply.github.com>
2020-09-06 21:48:47 +0800

    test

--- src/layouts/summary/summary-page.js
+++ src/layouts/summary/summary-page.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Button from 'devextreme-react/button';
+import DateTime from '../../components/form/date-time';
 
 import './summary-page.scss';
 
@@ -20,12 +21,13 @@ const SummaryPage = () => {
                    <div id="summaryChart" className="chart-3" style={{height: '300px'}} />
                    <div id="summaryWord" className="text-block">
                        <ul>
-                           <li>test123</li>
+                           <li>test123 <a href="./spotfireTool/JavaScriptApiTest.htm" target="_self">Test</a></li>
                            <li>test56789</li>
                            <li>test321987455r56y56</li>
                         </ul>
                     </div>
                </div>
+               <DateTime />
             </div>
        </Fragment>
    )
